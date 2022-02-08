5c80accf836a5e328bbbc975993d64e2b6063298
Andre Cesar <andrmgc@gmail.com>
2022-02-08 09:07:32 -0300

    Realizado teste do fetchItem, utilizando mesma logica do fetchProducts, acrescentado ao a funcao fetchItem try catch conforme solicitado

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,9 +1,13 @@
 const fetchItem = async (parameter = 'MLB1341706310') => {
   // seu código aqui
+  try {
   const getApi = `https://api.mercadolibre.com/items/${parameter}`;
   const apiResponse = await fetch(getApi);
   const result = await apiResponse.json();
   return result;
+  } catch (error) {
+    throw new Error('You must provide and url');
+  }
 };
 if (typeof module !== 'undefined') {
   module.exports = {
--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -4,5 +4,21 @@ const item = require('../mocks/item');
 
 describe('2 - Teste a função fecthItem', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  it('testa se é uma função', () => {
+    expect(typeof fetchItem).toMatch('function');
+  });
+  it('testa se a função foi chamada', async () => {
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalled();
+  });
+  it('testa se a url solicitada foi utilizada pelo fetch', async () => {
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalledWith("https://api.mercadolibre.com/items/MLB1615760527");
+  });
+  it('testa se retorno da função é um objeto', async () => {
+    expect(fetchItem('MLB1615760527')).resolves.toMatchObject(item);
+  });
+  it('testa se chamar a função sem argumentos retorna erro', async () => {
+    await expect(fetchItem()).rejects.toEqual(new Error("You must provide an url"));
+  });
 });
